147ee1eb7d9df9c66e38077018eb51744630300e
Eric Lin <zxe2124@gmail.com>
2023-09-20 22:33:12 +0800

    add: appendMessage function and render to view

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const socket = io('http://localhost:3000');
+const messageContainer = document.getElementById('message-container');
+const messageForm = document.getElementById('send-container');
+const messageInput = document.getElementById('message-input');
 
 socket.on('Chatroom-message', data => {
-    console.log(data);
+    appendMessage(data);
 });
+//when submit is triggered, get text input and send it to server then reset to ''
+messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-message', message);
+    messageInput.value = '';
+})
+
+//append message text on view
+function appendMessage(message) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+}
