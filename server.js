8346f29637f3f1fb624bd21bccfccfa57bb90523
Eric Lin <zxe2124@gmail.com>
2023-10-11 22:42:38 +0800

    add: connect to mongodb in server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express =  require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
+const server = require("http").Server(app);
+const mongoose = require("mongoose");
 //decode special html character
-const entities = require('entities');
+const entities = require("entities");
 const io = require("socket.io")(server, {
   //create server and handle CORS issue
   cors: {
@@ -10,65 +11,83 @@ const io = require("socket.io")(server, {
     methods: ["GET", "POST"],
   },
 });
+//connect to MongoDB
+const MONGO_URL =
+  "mongodb+srv://admin:admin@cluster0.yq0rd2d.mongodb.net/user?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready~");
+  //Start the server only after MongoDB is connected
+  //listen on port 3000
+  server.listen(3000, () => {
+    console.log("Server listening on port 3000");
+  });
+});
+
+mongoose.connection.on("error", () => {
+  console.error(err);
+});
+
+mongoose.connect(MONGO_URL);
 
 //ejs config
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 const rooms = {};
 
 //route setting
-app.get('/', (req, res) => {
-  res.render('index', {rooms: rooms})
+app.get("/", (req, res) => {
+  res.render("index", { rooms: rooms });
 });
 
-app.post('/room', (req, res) => {
-  
+app.post("/room", (req, res) => {
   if (rooms[req.body.room] != null) {
-    return res.redirect('/');
-  };
+    return res.redirect("/");
+  }
   rooms[req.body.room] = { users: {} };
-  io.emit('room-created', req.body.room);
+  io.emit("room-created", req.body.room);
   res.redirect(`/${req.body.room}`);
-})
+});
 
-app.get('/:room', (req, res) => {
+app.get("/:room", (req, res) => {
   //if the room doesnt exist return to main page
   if (rooms[req.params.room] == null) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
-  res.render('room', { roomName: req.params.room })
+  res.render("room", { roomName: req.params.room });
 });
-//listen on port 3000
-server.listen(3000);
 
 io.on("connection", (socket) => {
-  socket.on('new-user join', (room, userName) => {
+  socket.on("new-user join", (room, userName) => {
     room = entities.decodeHTML(room);
     //try
-    console.log('Room name received:', room);
-    console.log('Current rooms:', rooms);
+    console.log("Room name received:", room);
+    console.log("Current rooms:", rooms);
     if (!rooms[room]) {
-      console.log('Error: Room does not exist:', room);
+      console.log("Error: Room does not exist:", room);
       return;
     }
     //try
     socket.join(room);
     rooms[room].users[socket.id] = userName;
-    socket.to(room).emit('user-connected', userName);
+    socket.to(room).emit("user-connected", userName);
   });
   socket.on("send-message", (room, message) => {
     room = entities.decodeHTML(room);
-    socket.to(room).emit("Chatroom-message", {message: message , userName: rooms[room].users[socket.id]});
+    socket.to(room).emit("Chatroom-message", {
+      message: message,
+      userName: rooms[room].users[socket.id],
+    });
   });
 
-  socket.on('disconnect', () => {
-    getUserRooms(socket).forEach(room => {
-      socket.to(room).emit('user-disconnected', rooms[room].users[socket.id])
+  socket.on("disconnect", () => {
+    getUserRooms(socket).forEach((room) => {
+      socket.to(room).emit("user-disconnected", rooms[room].users[socket.id]);
       delete rooms[room].users[socket.id];
-    })
+    });
   });
 });
 
@@ -76,7 +95,7 @@ function getUserRooms(socket) {
   return Object.entries(rooms).reduce((roomNames, [roomName, room]) => {
     if (room.users[socket.id] != null) roomNames.push(roomName);
     return roomNames;
-  }, [])
+  }, []);
 }
 //test
 module.exports = server;
