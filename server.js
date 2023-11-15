6145d11b0ec8804396b6a74b55a9b3194968bc30
Eric Lin <zxe2124@gmail.com>
2023-11-15 21:09:01 +0800

    refactor: coding style

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const https = require("https");
 const express = require("express");
 const app = express();
-const server = require("https").createServer({
+const server = https.createServer({
   key: fs.readFileSync("key.pem"),
   cert: fs.readFileSync("cert.pem"),
 }, app);
