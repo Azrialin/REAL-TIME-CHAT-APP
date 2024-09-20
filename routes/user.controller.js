ba149c4c852fdbf68ce9d835ed68596b94aeeca1
Eric Lin <zxe2124@gmail.com>
2024-09-20 22:59:11 +0800

    refactor: user.controller.js and userApi.controller.js, so it's more readable

--- routes/user.controller.js
+++ routes/user.controller.js
@@ -15,14 +15,15 @@ const register = async (req, res) => {
             useremail: req.body.useremail,
             password: req.body.password,
         }
+
         const createdUser = await userModel.createUser(newUser);
         if (createdUser) {
             //TODO:目前註冊/登入成功只會直接轉頁，後面要加session去紀錄使用者，不然註冊/登入沒意義
-            res.redirect('/home');
-        } else {
-           // Redirect to the registration page with an error message
-           return res.redirect('/login?errorMessage=User creation failed');
+            return res.redirect('/home');
         }
+
+        // Redirect to the registration page with an error message
+        return res.redirect('/login?errorMessage=User creation failed');
     } catch (error) {
         console.error('Registration error:', error);
         // Redirect to the registration page with an error message
@@ -35,20 +36,19 @@ const register = async (req, res) => {
 const getUser = async (req, res) => {
     try {
         const user = await userModel.findUserByEmail(req.body.useremail);
-        if (user) {
-            //check password
-            if (user.password === req.body.password) {
-                //if useremail & password are correct redirect to home route
-                res.redirect('/home');
-            } else {
-                //if password are not correct redirect to home route with errorMessage
-                return res.redirect('/login?errorMessage=Invalid password');
-            }
-        } else {
+        if (!user) {
             // if useremail are not correct redirect to home route with errorMessage
-            // res.status(404).json({ message: 'User not found' });
             return res.redirect('/login?errorMessage=User not found');
         }
+
+        //check password
+        if (user.password !== req.body.password) {
+            //if password are not correct redirect to home route with errorMessage
+            return res.redirect('/login?errorMessage=Invalid password');
+        }
+        
+        //if useremail & password are correct redirect to home route
+        return res.redirect('/home');
     } catch (error) {
         console.error('Login error:', error);
         return res.redirect('/login?errorMessage=An error occurred, please try again');
--- routes/userApi.controller.js
+++ routes/userApi.controller.js
@@ -14,14 +14,15 @@ const register = async (req, res) => {
             useremail: req.body.useremail,
             password: req.body.password,
         }
+
         const createdUser = await userModel.createUser(newUser);
-        if (createdUser) {
-            res.status(201).json(createdUser);//201 is created status code
-        } else {
-            res.status(500).json({ message: 'User creation failed '});
+        if (!createdUser) {
+            return res.status(500).json({ message: 'User creation failed '});
         }
+
+        return res.status(201).json(createdUser);//201 is created status code
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        return res.status(500).json({ error: error.message });
     }
 };
 
@@ -30,16 +31,16 @@ const register = async (req, res) => {
 const getUser = async (req, res) => {
     try {
         const user = await userModel.findUserByEmail(req.body.useremail);
-        if (user) {
-            //check password
-            if (user.password === req.body.password) {
-                res.status(200).json(user);
-            } else {
-                res.status(401).json({ message: 'Invalid password' });
-            }
-        } else {
+        if (!user) {
             res.status(404).json({ message: 'User not found' });
         }
+
+        //check password
+        if (user.password !== req.body.password) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        
+        return res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
